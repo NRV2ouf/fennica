20a274cd176934737e3fd1521999c3ee2a64ac46
NRV2ouf <NRV2ouf@users.noreply.github.com>
2023-05-03 07:36:07 +0000

    Update gh-pages to output generated at 12f6e85

--- inst/examples/physical_extent.R
+++
@@ -1,7 +0,0 @@
-#df.tmp <- bibliographica::polish_physical_extent(df.orig[[field]], verbose = TRUE)
-rm.dim.file <- "remove_dimension.csv"
-df.tmp <- polish_physical_extent(df.orig[[field]], verbose = TRUE, rm.dim.file = rm.dim.file)
-
-# Vol number and count fields could not be extracted from physical_extent field in Fennica - remove
-df.tmp$volcount <- NULL
-df.tmp$volnumber <- NULL
